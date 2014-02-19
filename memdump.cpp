2805709551b43f80d0fb9e59d94da7d88503ac73
Derrick W. Turk <dwt@terminusdatascience.com>
2014-02-18 21:40:21 -0600

    Added ability to specify output file on command line, becausee Windows insists on being terrible.

--- memdump.cpp
+++ memdump.cpp
@@ -3,16 +3,19 @@
 #include "process_memory.h"
 
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <cerrno>
 #include <cstddef>
+#include <cstdio>
 
 std::size_t dump_process_memory(DWORD pid);
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        std::cerr << "Usage: " << (argc ? argv[0] : "memdump") << " <pid>\n";
+    if (argc < 2 || argc > 3) {
+        std::cerr << "Usage: " << (argc ? argv[0] : "memdump")
+            << " <pid> [output-file]\n";
         return 0;
     }
 
@@ -22,13 +25,29 @@ int main(int argc, char** argv)
         return 0;
     }
 
+    std::ostream *os = nullptr;
+    std::ofstream out;
+    if (argc < 3) {
+        FILE *bin_out = std::freopen(nullptr, "wb", stdout);
+        if (bin_out != stdout) {
+            std::cerr << "Warning: unable to reopen standard out in "
+                "binary mode.\n";
+        }
+        os = &std::cout;
+    } else {
+        out.open(argv[2], std::ios_base::out | std::ios_base::binary);
+        if (!out) {
+            std::cerr << "Unable to open " << argv[2]
+                << " for binary output.\n";
+            return 0;
+        }
+        os = &out;
+    }
+
     try {
         process_memory_iterator i(pid), end;
-
-        while (i != end) {
-            std::cout << *i;
-            ++i;
-        }
+        for (; i != end; ++i)
+            (*os) << *i;
     } catch (const std::runtime_error& e) {
         std::cerr << "Error: " << e.what() << '\n';
     }
